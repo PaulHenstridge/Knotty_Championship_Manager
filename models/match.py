efc83837df796af4e8e1929bdb3bb5681e8b2d22
Paul Henstridge <paulhenstridge@gmail.com>
2023-08-04 14:20:48 +0100

    improved game logic,match reports

--- models/match.py
+++ models/match.py
@@ -1,3 +1,8 @@
+import random
+import math
+from utils.action_reports import *
+
+
 class Match:
     def __init__(self, team1, team2, completed, winner=None, id=None):
         self.team1 = team1
@@ -5,16 +10,62 @@ class Match:
         self.completed = completed
         self.winner = winner
         self.id = id
+        self.match_score = [0, 0]
+        self.penalties = False
+        self.report = []
 
     def play(self):
         winner = None
         self.team1.matches_played += 1
         self.team2.matches_played += 1
 
-        if self.team1.attack > self.team2.defence:
+        team1_opportunities = (self.team1.defence + self.team1.defence) / 10
+        team2_opportunities = (self.team2.defence + self.team2.defence) / 10
+
+        for opportunity in range(round(team1_opportunities)):
+            chance_of_scoring = 50 + (self.team1.attack - self.team2.defence)
+            shot = random.random() * 100
+            if shot > chance_of_scoring:
+                self.match_score[0] += 1
+                self.generate_report(self.team1, self.team2, True)
+            else:
+                self.generate_report(self.team1, self.team2, False)
+
+        for opportunity in range(round(team2_opportunities)):
+            chance_of_scoring = 50 + (self.team2.attack - self.team1.defence)
+            shot = random.random() * 100
+            if shot > chance_of_scoring:
+                self.match_score[1] += 1
+                self.generate_report(self.team2, self.team1, True)
+            else:
+                self.generate_report(self.team2, self.team1, False)
+        print("match score", self.match_score)
+        # draw setled by penalties
+        if self.match_score[0] == self.match_score[1]:
+            self.match_score[round(random.random())] += 1
+            self.penalties = True
+
+        if self.match_score[0] > self.match_score[1]:
             winner = self.team1
         else:
             winner = self.team2
         winner.wins += 1
         self.winner = winner
         self.completed = True
+        # random.shuffle(self.report)
+
+    def generate_report(self, team, other_team, goal):
+        if goal:
+            message = score_reports[
+                math.floor(random.random() * len(score_reports))
+            ].format(
+                team_name=team.name,
+                other_team_name=other_team.name,
+                score=self.match_score,
+            )
+        else:
+            message = miss_reports[
+                math.floor(random.random() * len(miss_reports))
+            ].format(team_name=team.name, other_team_name=other_team.name)
+
+        self.report.append(message)
---
+++ utils/action_reports.py
@@ -0,0 +1,17 @@
+score_reports = [
+    "{team_name} have scored a stunning goal! - {score}",
+    "A well worked goal from {team_name} - {score}",
+    "{team_name} split the {other_team_name} defence and scored a classic.  - {score}",
+    "{team_name} on the attack! He headbutts a {other_team_name} defender... GOAL!!!  - {score}",
+    "{team_name} attacking from wide, early ball to the striker, GOAL!  - {score}",
+    "{team_name} try a long range shot, and its in!  what a goal!  - {score}",
+]
+
+miss_reports = [
+    "{team_name} scramble to keep {other_team_name} from scoring",
+    "{other_team_name} attack but {team_name}'s defense are up to it",
+    "{other_team_name} have ther ball, they SHOOT... but {team_name} block the shot",
+    "Solid defence from {team_name} to clear the danger",
+    "{team_name} slice down the {other_team_name} attacker with a brutal chop to the legs",
+    "Surely a goal for {other_team_name}... its WIDE!  how did he miss that?",
+]
